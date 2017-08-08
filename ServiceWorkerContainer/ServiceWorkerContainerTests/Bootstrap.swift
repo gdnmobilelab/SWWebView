0bd0e3c1c2b4c5b63ac18f746cb015eb82d2eab9
Alastair Coote <alastair.coote@gmail.com>
2017-08-08 14:17:28 -0400

    restore tests to serviceworkercontainer

---
+++ ServiceWorkerContainer/ServiceWorkerContainerTests/Bootstrap.swift
@@ -0,0 +1,33 @@
+//
+//  Bootstrap.swift
+//  ServiceWorkerContainerTests
+//
+//  Created by alastair.coote on 15/06/2017.
+//  Copyright © 2017 Guardian Mobile Innovation Lab. All rights reserved.
+//
+
+import Foundation
+import ServiceWorkerContainer
+import XCTest
+import ServiceWorker
+
+class TestBootstrap: NSObject {
+
+    override init() {
+        super.init()
+        
+        Log.error = { NSLog($0) }
+        Log.warn = { NSLog($0) }
+        Log.info = { NSLog($0) }
+        Log.debug = { NSLog($0) }
+        
+        do {
+            CoreDatabase.dbPath = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("testDB", isDirectory: true)
+            if FileManager.default.fileExists(atPath: CoreDatabase.dbPath!.path) == false {
+                try FileManager.default.createDirectory(at: CoreDatabase.dbPath!, withIntermediateDirectories: true, attributes: nil)
+            }
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
+}
